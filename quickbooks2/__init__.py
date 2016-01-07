7de0e1d4a7ab83a67d64c8f35900bacc34c1a978
Andrew Sauber <a3sauber@gmail.com>
2016-01-07 13:54:40 -0500

    add setup.py

---
+++ quickbooks2/__init__.py
@@ -0,0 +1,1 @@
+from quickbooks2 import QuickBooks
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+setup(
+    name = "quickbooks2",
+    version = "0.1",
+    packages = find_packages(),
+)
